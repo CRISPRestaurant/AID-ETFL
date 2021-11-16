1c5aa166653689bbe268021bc0abc8c2b6c38709
Siddhartha Peri <siddhu.classwork@gmail.com>
2021-11-15 18:18:28 -0600

    changes

--- etfl/core/memodel.py
+++ etfl/core/memodel.py
@@ -1435,7 +1435,7 @@ class MEModel(LCSBModel, Model):
 
         # Get the RNAP assigned to this transcription
         RNAPi_hat = rnap_usage_vars.get_by_id(the_gene.id)
-        print(RNAPi_hat.id)
+        print(the_gene.transcribed_by[0].id)
 
         # Get the number of loci
         n_loci = the_gene.copy_number
